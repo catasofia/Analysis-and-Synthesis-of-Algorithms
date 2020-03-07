5f7d3719a63f0a9352aeeb83840fb484c68e7e16
nelsontr <trindade.nelson99@gmail.com>
2020-03-07 17:52:05 +0000

    Adding Ideas

--- Project1.cpp
+++ Project1.cpp
@@ -1,2 +1,16 @@
 /*Project 1*/
 
+/*
+ - Criar Grafo - Vertices dados na primeira linha
+ - Cada Vertice com o seu id e dps a nota esperada
+ - Cada um terá tambem a sua lista de antecessores (?)
+ 
+ - No input
+ - A medida que se da a nota esperada, cria-se os vertices
+ - A medida que se da as conexões, coloca-se o antecessor
+    e atualiza-se a nota esperada para nota maxima entre ambos
+
+ - Complexidade? o(n)??
+ - Variaveis globais? -> Classe Grafo?
+ - Memoria e Tempo limitados
+*/
