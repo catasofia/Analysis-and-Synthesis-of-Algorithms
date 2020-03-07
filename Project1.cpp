59f91a595d5d9ce73397c6adcba495de4bbc64f6
catasofia <catarina.s.sousa@hotmail.com>
2020-03-07 19:00:22 +0000

    Cata Ideas

--- Project1.cpp
+++ Project1.cpp
@@ -13,4 +13,15 @@
  - Complexidade? o(n)??
  - Variaveis globais? -> Classe Grafo?
  - Memoria e Tempo limitados
+
+-------------------------------------------------------------------------
+ -Ter classe Vertice que tem uma lista dos vertices que lhe são adjacentes
+ -Aplicar Algoritmo de Tarjan e em vez de comparar o LOW e colocar o menor,
+   comparar o valor do vértice (nota do teste) e colocar o maior;
+ -Complexidade -> O(V + E)
+ -Ter um atributo com o id do parent;
+
+ ->Função "push_back" adiciona no fim da lista  --- penso que O(1) "amortized";
+
+
 */
