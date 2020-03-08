e7db0c49b51cce450a4a78d1b4661987f6ba7f48
Nelson Trindade <nelsontr@users.noreply.github.com>
2020-03-08 23:08:13 +0000

    adding runTest.sh

---
+++ runTests.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#Checks if there are 2 elements after the executable
+if [ ! $# -eq 2 ];then
+	echo "Wrong format!"
+	echo "Correct form: ./runTests.sh inputFolder outputFolder"
+	exit 1
+fi
+
+#Inputs
+inputdir=$1
+outputdir=$2
+
+#Scrpit
+if [ ! -d "tecnicofs-*" ]; then
+	make all | grep !""
+fi
+
+mkdir -p $2
+
+
+for inputFile in "$inputdir"/*
+do
+	echo "InputFile = ​${inputFile/"$inputdir/"/""}"
+	auxFile=${inputFile/"$inputdir"/$outputdir}
+	auxFile=${auxFile%.*}
+	outFile="$auxFile.out"
+	./p1Exe <$inputFile >$outFile
+
+done
+
+#make clean | grep !""
+exit 0
