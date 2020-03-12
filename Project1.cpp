67de63a92357474799d061ea9220c5ef598acb84
Nelson Trindade <nelsontr@users.noreply.github.com>
2020-03-12 17:31:22 +0000

    Update Project1.cpp

--- Project1.cpp
+++ Project1.cpp
@@ -8,140 +8,87 @@
 using namespace std;
 
 /* Classes */
-class Vertex
-{
-private:
-  int _Id;
-  int _grade;
-  vector<Vertex *> _connections;
-  int _discoveryTime;
-  int _low;
-  bool _visited;
+class Vertex {
+  private:
+    int _Id;
+    int _grade;
+    vector<Vertex *> _connections;
+    int _discoveryTime;
+    int _low;
+    bool _visited;
 
-public:
-  Vertex() {
-    _visited = false;
-    _discoveryTime = -1;
-    _low = 0;
-  }
-  ~Vertex() {}
-
-  int getGrade()
-  {
-    return _grade;
-  }
-
-  int getId()
-  {
-    return _Id;
-  }
-
-  void setGrade(int newGrade)
-  {
-    _grade = newGrade;
-  }
-
-  void setId(int id)
-  {
-    _Id = id;
-  }
-
-  int getDiscoveryTime(){
-    return _discoveryTime;
-  }
-
-  int getLow(){
-    return _low;
-  }
-
-  bool isVisited(){
-    return _visited;
-  }
-
-  void setVisited(){
-    _visited = true;
-  }
-
-  void setDiscoveryTime(int disc){
-    _discoveryTime = disc;
-  }
-
-  void setLow(int low){
-    _low = low;
-  }
-
-  void addConnections(Vertex *v)
-  {
-    _connections.push_back(v);
-  }
-
-  vector<Vertex *> getAdjacents()
-  {
-    return _connections;
-  }
-
-  bool hasConnection(int id)
-  {
-    for (Vertex *v : _connections)
-    {
-      if (v->getId() == id)
-        return true;
+  public:
+    Vertex() {
+      _visited = false;
+      _discoveryTime = -1;
+      _low = 0;
     }
-    return false;
-  }
-  
-  bool hasChild()
-  {
-    return !(_connections.empty());
-  }
+    ~Vertex() {}
+
+    int getGrade() { return _grade; }
+
+    int getId() { return _Id; }
+
+    void setGrade(int newGrade) { _grade = newGrade; }
+
+    void setId(int id) { _Id = id; }
+
+    int getDiscoveryTime() { return _discoveryTime; }
+
+    int getLow() { return _low; }
+
+    bool isVisited() { return _visited; }
+
+    void setVisited() { _visited = true; }
+
+    void setDiscoveryTime(int disc) { _discoveryTime = disc; }
+
+    void setLow(int low) { _low = low; }
+
+    void addConnections(Vertex *v) { _connections.push_back(v); }
+
+    vector<Vertex *> getAdjacents() { return _connections; }
+
+    bool hasConnection(int id) {
+      for (Vertex *v : _connections)
+        if (v->getId() == id)
+          return true;
+      return false;
+    }
+    
+    bool hasChild() { return !(_connections.empty()); }
 };
 
-class Graph
-{
-private:
-  int _numVertexes;
-  Vertex *_vertexes;
+class Graph {
+  private:
+    int _numVertexes;
+    Vertex *_vertexes;
 
-public:
-  Graph() {}
-  Graph(int vertexes)
-  {
-    _vertexes = new Vertex[vertexes];
-    _numVertexes = vertexes;
-  }
-  ~Graph()
-  {
-    delete _vertexes;
-  }
+  public:
+    Graph() {}
+    Graph(int vertexes) {
+      _vertexes = new Vertex[vertexes];
+      _numVertexes = vertexes;
+    }
+    ~Graph() { delete _vertexes; }
 
-  int getGrade(int num)
-  {
-    return _vertexes[num].getGrade();
-  }
+    int getGrade(int num) { return _vertexes[num].getGrade(); }
 
-  void newVert(int id)
-  {
-    int grade = 0;
-    if (scanf("%d", &grade) != 1)
-      printf("ERRO!\n");
-    _vertexes[id].setId(id);
-    _vertexes[id].setGrade(grade);
-  }
+    void newVert(int id) {
+      int grade = 0;
+      if (scanf("%d", &grade) != 1)
+        printf("ERRO!\n");
+      _vertexes[id].setId(id);
+      _vertexes[id].setGrade(grade);
+    }
 
-  Vertex *getVertex(int id)
-  {
-    return &_vertexes[id];
-  }
+    Vertex *getVertex(int id) { return &_vertexes[id]; }
 
-  int getNumVectors()
-  {
-    return _numVertexes;
-  }
+    int getNumVectors() { return _numVertexes; }
 
-  void addConnection(int id, int idConnection)
-  {
-    _vertexes[id].addConnections(&_vertexes[idConnection]);
-  }
+    void addConnection(int id, int idConnection) {
+      _vertexes[id].addConnections(&_vertexes[idConnection]);
+    }
 };
 
 /* Global Variable */
@@ -153,7 +100,7 @@ int _time = 0;
 
 /* Functions */
 
-void tarjan(int id, bool visited[]){
+void tarjan(int id, bool visited[]) {
   
   Vertex* vertex = _g->getVertex(id);
   vertex->setDiscoveryTime(++_time);
@@ -194,8 +141,7 @@ void tarjan(int id, bool visited[]){
   }
 }
 
-void parseCommandLine()
-{
+void parseCommandLine() {
   int id = 0, idConnection = 0;
   int num_vert = 0, num_edges = 0;
 
@@ -216,20 +162,16 @@ void parseCommandLine()
   _g = new Graph(num_vert); //Inicialize graph
 
   for (int i = 1; i <= num_vert; i++) //Set Vertices with grade
-  {
     _g->newVert(i - 1);
-  }
 
-  for (int i = 1; i <= num_edges; i++) //Set relations between
-  {
+  for (int i = 1; i <= num_edges; i++) {//Set relations between
     if (scanf("%d %d", &id, &idConnection) != 2)
       fprintf(stderr, "Error");
     _g->addConnection(id - 1, idConnection - 1);
   }
 }
 
-void max(Vertex *a, Vertex *b)
-{
+void max(Vertex *a, Vertex *b) {
   if (a->getGrade() > b->getGrade())
     b->setGrade(a->getGrade());
 
@@ -237,8 +179,7 @@ void max(Vertex *a, Vertex *b)
     max(b,a);
 }
 
-void findPath(int v, bool visited[])
-{
+void findPath(int v, bool visited[]) {
   visited[v] = true;
   Vertex *vertex = _g->getVertex(v);
   for (Vertex *adjVertex : vertex->getAdjacents())
@@ -249,25 +190,21 @@ void findPath(int v, bool visited[])
   path.insert(path.begin(), v);
 }
 
-void propaga(int v)
-{
+void propaga(int v) {
   while ((unsigned) (v + 1) < path.size() && 
   _g->getVertex(path[v])->hasConnection(path[v + 1])){
     max(_g->getVertex(path[v+1]), _g->getVertex(path[v]));
     propaga(v + 1);
-    }
+  }
   if ((v - 1) >= 0)
     max(_g->getVertex(path[v]), _g->getVertex(path[v-1]));
-
-
   path.erase(path.begin() + v);
 }
 
 
 
 
-void DFS()
-{
+void DFS() {
   bool *visited = new bool[_g->getNumVectors()];
 
   for (int i = 0; i < _g->getNumVectors(); i++)
@@ -276,6 +213,7 @@ void DFS()
   for (int i = 0; i < _g->getNumVectors(); i++)
     if (!visited[i])
       tarjan(i, visited);
+
   /* for(auto i: path)
     printf("%d\t", i);
   printf("\n"); */
@@ -287,8 +225,7 @@ void DFS()
     propaga(0);
 }
 
-void output()
-{
+void output() {
   /* for (auto i : path)
     printf("%d", i);
   cout << "Elementos" << path.size() << "\n"; */
@@ -296,8 +233,7 @@ void output()
     printf("%d\n", _g->getGrade(i));
 }
 
-int main()
-{
+int main() {
   parseCommandLine();
 
   DFS();
